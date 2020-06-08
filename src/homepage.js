34d57434495562d4afa8105e2246454e4635659a
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-06-08 15:21:25 -0400

    [ADD] Initial App Setup

---
+++ src/homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className="menu-container">
+            <div className="menu-item">
+                <div className='content'>
+                    <h1>Category</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
