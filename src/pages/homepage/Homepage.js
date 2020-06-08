6c7bdf8a7cfc2ce41ba4eb1c7c1e4e46533a8f27
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-06-08 17:04:00 -0400

    [UPDATE] Product Homepage

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage-style.scss'
+import Directory from '../../components/directory/Directory'
+const Homepage = () => (
+    <div className='homepage'>
+        <h1 className='intro'>FIND YOUR COLLECTION</h1>
+        <div className='menu'>
+            <Directory></Directory>
+        </div>
+    </div >
+);
+
+export default Homepage;
