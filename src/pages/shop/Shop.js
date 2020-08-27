44724d3926bbb33a7a0349d65e8e24bdb9255f4a
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-08-26 21:33:26 -0400

    Added initial shop page + preview components

---
+++ src/pages/shop/Shop.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import SHOP_DATA from './ShopData'
+import CollectionPreview from '../../components/collection-preview/CollectionPreview'
+
+class Shop extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
+
+
+    render() {
+        const { collections } = this.state
+        return (
+            <div className="shop-page">
+                <h1>Shop Page!</h1>
+                {
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))}
+            </div>
+        )
+    }
+}
+
+export default Shop
