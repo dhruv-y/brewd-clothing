8988300890f9dfe2e6eb3bea1e5e829a1328e2c1
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2021-03-24 00:25:54 -0400

    updated App files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 import Homepage from './pages/homepage/Homepage'
 import Shop from './pages/shop/Shop'
-import { Switch, Route } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={Shop} />
